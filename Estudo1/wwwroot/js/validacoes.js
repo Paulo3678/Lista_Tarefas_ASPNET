ba63d98ee47ea03d0f694065bb1016b4c9354215
Paulo3678 <ph7811001@fmail.com>
2023-04-19 16:14:16 -0300

    :sparkles: Validação do formulário no front-end

--- Estudo1/wwwroot/js/validacoes.js
+++ Estudo1/wwwroot/js/validacoes.js
@@ -9,23 +9,33 @@ $("#formulario-tarefas").on("submit", e => {
             id: "#descricaoTarefa"
         }
     ]);
-
 });
+
+
+document.querySelectorAll(".form-control").forEach(input => {
+    input.addEventListener("input", e => {
+        const valor_digitado = input.value;
+        if (valor_digitado !== "" && valor_digitado !== null) {
+            if ($(`#${input.id}`).prev().hasClass("text-danger")) {
+                $(`#${input.id}`).prev().remove();
+            }
+        }
+    });
+});
+
 
 function CamposParaValidar(campos) {
 
     campos.forEach(campo => {
         const campo_element = $(campo.id);
         const valor_campo = $(campo.id).val();
-        console.log(campo_element.next());
+
         if (valor_campo == null || valor_campo == "") {
-            if (!$(campo.id).next().hasClass("text-danger")) {
+            if (!$(campo.id).prev().hasClass("text-danger")) {
                 let novo = $("<span class='text-danger d-block'>Esse campo eh requerido!</span > ");
                 novo.insertBefore(campo_element);
             }
         }
-
-        console.log($(campo.id).next());
 
     });
 }
